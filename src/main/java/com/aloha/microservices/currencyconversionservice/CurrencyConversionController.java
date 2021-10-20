0506d30a75c7c386e27ca8e6fb67d1f3317c5f3f
mli <menghua.li@westjet.com>
2021-10-19 22:01:50 -0400

    use feign to call external service

--- src/main/java/com/aloha/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/aloha/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -9,16 +9,21 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RequestMapping("/currency-conversion")
 @RestController
 public class CurrencyConversionController {
 
     @Autowired
     private Environment environment;
 
-    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    @Autowired
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    @GetMapping("/resttemplate/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convert(@PathVariable("from") String from, @PathVariable("to") String to,
             @PathVariable("quantity") BigDecimal quantity) {
         Map<String, String> uriParams = new HashMap<String, String>();
@@ -27,6 +32,13 @@ public class CurrencyConversionController {
         ResponseEntity<CurrencyExchange> resp = new RestTemplate().getForEntity(
                 "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyExchange.class, uriParams);
         CurrencyExchange exchg = resp.getBody();
+        return new CurrencyConversion(exchg, quantity, environment.getProperty("local.server.port"));
+    }
+
+    @GetMapping("/feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion convertFeign(@PathVariable("from") String from, @PathVariable("to") String to,
+            @PathVariable("quantity") BigDecimal quantity) {
+        CurrencyExchange exchg = currencyExchangeProxy.getCurrencyExchange(from, to);
         return new CurrencyConversion(exchg, quantity, environment.getProperty("local.server.port"));
     }
 
---
+++ src/main/java/com/aloha/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.aloha.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange getCurrencyExchange(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
