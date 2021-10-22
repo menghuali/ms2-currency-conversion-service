6a3db2576a7f1fcce808cb84058ffd1e0f380940
mli <menghua.li@westjet.com>
2021-10-21 21:55:04 -0400

    resolve code warnings

--- src/main/java/com/aloha/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/aloha/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
